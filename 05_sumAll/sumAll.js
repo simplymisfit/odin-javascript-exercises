2899a05505aa82479eeb073a1fcb73216246890d
msft <63207978+simplymisfit@users.noreply.github.com>
2022-06-12 10:37:59 +0200

    x

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(firstArg, secondArg) {
+    let sum = 0;
+    if (firstArg < 0 || secondArg < 0) return "ERROR";
+    if (typeof firstArg != "number" || typeof secondArg != "number") return "ERROR";
 
+    if (firstArg > secondArg) {
+        let temp = firstArg;
+        firstArg = secondArg;
+        secondArg = temp;
+    }
+    for (let i = firstArg; i <= secondArg; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
