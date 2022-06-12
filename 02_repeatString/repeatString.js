78f5acaebd286ffaa1064ac5ebc7b9d0a2f44097
msft <63207978+simplymisfit@users.noreply.github.com>
2022-06-12 10:13:42 +0200

    x

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (text, count) {
+    if (count < 0) return "ERROR";
+    let output = "";
+    for (let i = 0; i < count; i++) {
+        output += text;
+    }
+    return output;
 };
 
 // Do not edit below this line
