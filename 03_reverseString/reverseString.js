a9a8f7716c42ca7c1e891b66b1707b0e07e88c89
msft <63207978+simplymisfit@users.noreply.github.com>
2022-06-12 10:18:03 +0200

    x

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let string = "";
+    for (let i = text.length - 1; i >= 0; i--) {
+        string += text[i];
+    }
+    return string;
 };
 
 // Do not edit below this line
